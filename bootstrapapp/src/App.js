6f308c7410ecaf7b95fe64d379fe80806d086574
Ashvin Kanzariya <ashvin@arhantsolutions.com>
2025-05-11 09:33:51 +0530

    Light Mode And dark Mode With UseState Hooks

--- bootstrapapp/src/App.js
+++ bootstrapapp/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import About from './Components/About';
 
 function App() {
   return (
     <>
       <Navbar title="First App" about="About" />
+      <About/>
       <div className="container my-3">
         <TextForm heading="Enter your text below" />
       </div>
